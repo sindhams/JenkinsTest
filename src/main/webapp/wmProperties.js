108775f85c33c306fe0693ede37874d2e90da8e6
Sai Sameera Grandhi <sameera.grandhi@wavemaker.com>
2017-10-10 07:55:19 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "JenkinsTest",
+  "homePage" : "Main",
+  "name" : "JenkinsTest",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
